16905794cc10cf728937d279f859bbefe7d614b9
Vera Kiprskaya <vera.malyutina@gmail.com>
2022-08-06 17:05:19 +0300

    Add project files.

---
+++ Lab 8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\veram\OneDrive\Documents\Interior design\Андрей Челюскина";
+            string[] files = Directory.GetFiles(path, "*",SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\veram\OneDrive\Documents\Revit\C#\Numbers.txt";
+            Random random = new Random();
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(random.Next(-10, 10));
+                }
+            }
+            int s = 0;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    s+=(Convert.ToInt32(sr.ReadLine()));
+                }
+            }        
+            Console.WriteLine(s); 
+            Console.ReadKey(); 
+        }
+    }
+}
